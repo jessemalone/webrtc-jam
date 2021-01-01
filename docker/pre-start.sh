7b2338e88dabbab9806910e25e198ebd7c4c5917
Jesse Malone <jesse.malone@gmail.com>
2021-01-01 12:12:26 -0500

    Avoid embedding turn server credentials in the docker image
      - Run webpack build during container startup to pickup runtime environment variables instead

--- docker/pre-start.sh
+++ docker/pre-start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# build the frontend app
+pushd /app/
+npm run build
+popd
 # Start the signalling server before the container launches nginx
 
 /app/server/app.py >>/app/app.py.log &
